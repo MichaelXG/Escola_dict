dcb3a6642e1bd77c9cb18f481752c672cba35195
Michael XG <michaelxg@gmail.com>
2024-05-16 20:55:45 -0300

    Ajustes

--- pages/Home/Create_Home.py
+++ pages/Home/Create_Home.py
@@ -19,23 +19,19 @@ def Create_Home():
         
         with col1_d:
             st.write("""
-    [PY-A07] Você foi contratado(a) para desenvolver um programa que gerencie um dicionário de alunos onde 
-    a chave deste dicionário é o número de matrícula dos próprios alunos. \n
-    O programa deve permitir adicionar, remover, atualizar e listar os alunos.
+                [PY-A07] Você foi contratado(a) para desenvolver um programa que gerencie um dicionário de alunos onde a chave deste dicionário é o número de matrícula dos próprios alunos. \n
+                O programa deve permitir adicionar, remover, atualizar e listar os alunos.
 
-    Para isso, você deve implementar um módulo que contém as seguintes funções:\n
+                Para isso, você deve implementar um módulo que contém as seguintes funções: \n
 
-    1 . AdicionarAluno(): Solicita ao usuário que digite o nome e o número de matrícula de um aluno 
-    e adicione-o ao dicionário de alunos. \n
+                1 . AdicionarAluno(): Solicita ao usuário que digite o nome e o número de matrícula de um aluno e adicione-o ao dicionário de alunos. \n
 
-    2 . RemoverAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e remove-o do 
-    dicionário de alunos. \n
+                2 . RemoverAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e remove-o do dicionário de alunos. \n
 
-    3. AtualizarAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e atualize 
-    o nome desse aluno no dicionário. \n
+                3. AtualizarAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e atualize o nome desse aluno no dicionário. \n
 
-    4. VerAlunos(): Lista todos os alunos cadastrados, exibindo o número de matrícula e o nome de cada um. \n
-""")
+                4. VerAlunos(): Lista todos os alunos cadastrados, exibindo o número de matrícula e o nome de cada um. \n
+            """)
         with col2_d:
             st.write('')
         
